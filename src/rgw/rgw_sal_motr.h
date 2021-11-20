77dfbf9407380b69b04552fcad9c872496cdca1e
Sining Wu <sining.wu@seagate.com>
2021-11-20 22:54:34 +0000

    Add bucket creation and listing a user's buckets (#2)
    Creating and listing a bucket works now. Commands:
    
        s3cmd -c rgw_s3cmd.cfg --no-ssl mb s3://bucket1
        s3cmd -c rgw_s3cmd.cfg --no-ssl ls s3://
    
    Signed-off-by: Sining Wu <sining.wu@seagate.com>

--- src/rgw/rgw_sal_motr.h
+++ src/rgw/rgw_sal_motr.h
@@ -122,6 +122,9 @@ protected:
       virtual int trim_usage(const DoutPrefixProvider *dpp, uint64_t start_epoch, uint64_t end_epoch) override;
 
       /* Placeholders */
+      int load_user_from_motr_idx(const DoutPrefixProvider *dpp,
+                                  RGWUserInfo& info, map<string, bufferlist> *attrs,
+                                  RGWObjVersionTracker *objv_tracker);
       virtual int load_user(const DoutPrefixProvider* dpp, optional_yield y) override;
       virtual int store_user(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) override;
       virtual int remove_user(const DoutPrefixProvider* dpp, optional_yield y) override;
@@ -660,7 +663,13 @@ protected:
       int open_idx(struct m0_uint128 *id, bool create, struct m0_idx *out);
       void close_idx(struct m0_idx *idx) { m0_idx_fini(idx); }
       int do_idx_op(struct m0_idx *, enum m0_idx_opcode opcode,
-                    vector<uint8_t>& key, vector<uint8_t>& val, bool update = false);
+		    vector<uint8_t>& key, vector<uint8_t>& val, bool update = false);
+
+      int do_idx_next_op(struct m0_idx *idx,
+                         vector<vector<uint8_t>>& key_vec,
+                         vector<vector<uint8_t>>& val_vec);
+      int next_query_by_name(string idx_name,
+                             vector<string>& key_str_vec, std::vector<bufferlist>& val_bl_vec);
 
       void index_name_to_motr_fid(string iname, struct m0_uint128 *fid);
       int open_motr_idx(struct m0_uint128 *id, struct m0_idx *idx);
