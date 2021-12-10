josephsawaya/ceph

mgr/rook, mgr/nfs: update rook orchestrator to create and use .nfs pool
This commit moves the functionality for creating the .nfs pool from the
nfs module to the rook module and makes the rook module use the .nfs
pool when creating an NFS daemon.

Signed-off-by: Joseph Sawaya <jsawaya@redhat.com>

src/pybind/mgr/rook/module.py
@@ -428,6 +428,9 @@ class RookOrchestrator(MgrModule, orchestrator.Orchestrator):
             return self.rook_cluster.rm_service('cephobjectstores', service_name)
         elif service_type == 'nfs':
             return self.rook_cluster.rm_service('cephnfses', service_name)
+        elif service_type == 'ingress':
+            self.log.info("{0} service '{1}' does not exist".format('ingress', service_name))
+            return 'The Rook orchestrator does not currently support ingress'
         else:
             raise orchestrator.OrchestratorError(f'Service type {service_type} not supported')
 
@@ -452,7 +455,7 @@ class RookOrchestrator(MgrModule, orchestrator.Orchestrator):
     @handle_orch_error
     def apply_nfs(self, spec):
         # type: (NFSServiceSpec) -> str
-        return self.rook_cluster.apply_nfsgw(spec)
+        return self.rook_cluster.apply_nfsgw(spec, self)
 
     @handle_orch_error
     def remove_daemons(self, names: List[str]) -> List[str]:

andriytk/ceph-old

src/pybind/mgr/rook/module.py
@@ -511,6 +511,9 @@ class RookOrchestrator(MgrModule, orchestrator.Orchestrator):
                 del self._drive_group_map[service_id]
                 self._save_drive_groups()
             return f'Removed {service_name}'
+        elif service_type == 'ingress':
+            self.log.info("{0} service '{1}' does not exist".format('ingress', service_id))
+            return 'The Rook orchestrator does not currently support ingress'
         else:
             raise orchestrator.OrchestratorError(f'Service type {service_type} not supported')
 
@@ -552,7 +555,7 @@ class RookOrchestrator(MgrModule, orchestrator.Orchestrator):
     @handle_orch_error
     def apply_nfs(self, spec):
         # type: (NFSServiceSpec) -> str
-        return self.rook_cluster.apply_nfsgw(spec)
+        return self.rook_cluster.apply_nfsgw(spec, self)
 
     @handle_orch_error
     def remove_daemons(self, names: List[str]) -> List[str]: